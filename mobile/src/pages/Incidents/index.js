367eb3d125cf71da828a54363855500abab8555c
Jean Carlo <jean@codebit.com.br>
2020-03-29 12:48:28 -0300

    Inserindo paginacao Incidents

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -11,6 +11,9 @@ import api from '../../services/api';
 export default function Incidents() {
     const [incidents, setIncidents] = useState([]);
     const [total, setTotal] = useState(0);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+    
     const navigation = useNavigation();
 
     function navigateToDetail(incident) {
@@ -18,10 +21,22 @@ export default function Incidents() {
     }
 
     async function loadIncidents() {
-        const response = await api.get('incidents');
+        if (loading) {
+            return;
+        }
 
-        setIncidents(response.data);
+        if(total > 0 && incidents.length === total) {
+            return;
+        }
+
+        setLoading(true);
+
+        const response = await api.get(`incidents?page=${page}`);
+
+        setIncidents([... incidents, ...response.data]);
         setTotal(response.headers['x-total-count']);
+        setPage(page + 1);
+        setLoading(false);
     }
 
     useEffect(() => {
@@ -45,6 +60,8 @@ export default function Incidents() {
                 data={incidents}
                 keyExtractor={incident => String(incident.id)}
                 showsVerticalScrollIndicator={false}
+                onEndReached={loadIncidents}
+                onEndReachedThreshold={0.2}
                 renderItem={({ item: incident }) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
